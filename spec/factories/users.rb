bcc9f4c26e6a537c65c2370da36e8aa6a61a6475
Norly <canariasnorly@gmail.com>
2017-04-13 21:43:00 +0800

    Fixes email attribute on User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:first_name){|n| "User#{n}"}
     last_name { %w{Canarias Cano Reyes Alas}.sample }
-    sequence(:email){|n| "User#{n}"}
+    sequence(:email){|n| "user#{n}@example.com"}
     encrypted_password User.new.send(:password_digest, 'password')
   end
 end
